d5eef917022ac1088d74f2c28a3d05c83a86ecc8
runmyweb <frederic.morel.ars@outlook.fr>
2023-02-09 12:29:18 +0400

    add test function

--- src/functions/testFunctions.js
+++ src/functions/testFunctions.js
@@ -1,4 +1,12 @@
-// create a array of 10 numbers
+// create an array of 10 numbers
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//create an array of 10 names
+const names = [ 'John', 'Jane', 'Mary', 'Bob', 'Peter', 'Paul', 'Paula', 'Laura', 'Jim', 'Tim' ];
+
+// create an function that returns the array of names sorted by alphabetical order
+const sortNames = (names) => {
+  return names.sort();
+}   
 
 
